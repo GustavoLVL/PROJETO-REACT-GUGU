d57bf42bd16a9b0fb4feb3b9ac108728f74cd393
GustavoLVL <gustavomirandaoficiallvl@gmail.com>
2026-03-25 23:40:00 +0000

    Gustavo111103

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,13 +1,13 @@
-
-import { useState } from 'react'
 import './App.css'
 import Atividade1 from './Atividades/atividade1'
 import Atividade2 from './Atividades/atividade2'
+
 function App() {
   return (
     <>
       <div>
-          <Atividade2/> 
+       
+        <Atividade2 />
       </div>
     </>
   )
--- my-app/src/Atividades/atividade2.jsx
+++ my-app/src/Atividades/atividade2.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Atividade2() {
+  const [nome, setNome] = useState('');  
+
   return (
     <div>
-      <h3>Exercício 1</h3>
-      <p>Este é o Exercício 1.</p>
+      <h3>Exercício 2</h3>
+      <p>Esta é a atividade 2.</p>
+
+      <label>Digite seu nome: </label>
+      <input 
+        type="text" 
+        value={nome}  
+        onChange={(e) => setNome(e.target.value)}  
+      />
+      
+      {}
+      <p>Olá {nome}</p>
     </div>
   );
 }
