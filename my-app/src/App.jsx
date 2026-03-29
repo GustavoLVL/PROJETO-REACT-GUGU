1a69cd9fc9823959d4404442542df0f94d9db0c3
GustavoLVL <gustavomirandaoficiallvl@gmail.com>
2026-03-29 18:12:43 +0000

    Gustavo29/032026

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,13 +1,15 @@
 import './App.css'
 import Atividade1 from './Atividades/atividade1'
 import Atividade2 from './Atividades/atividade2'
+import AtividadeNova from './Atividades/atividadeNova'
 
 function App() {
   return (
     <>
       <div>
-       <Atividade1 />yui
-        <Atividade2 />
+       <Atividade1 />
+       <Atividade2 />
+        <AtividadeNova />
       </div>
     </>
   )
